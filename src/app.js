f1d30f32d9be9de4f3f14f735b430c4fcca9f43a
AbdullahFoqha <Aboodfoqha@outlook.com>
2022-06-13 02:01:47 -0500

    adding mdb string

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CORS);
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 app.use(helmet());
